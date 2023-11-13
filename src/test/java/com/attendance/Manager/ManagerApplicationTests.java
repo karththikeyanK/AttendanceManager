e4d1d330c8b15ecad711103c950a49a122b824e1
karththikeyanK <kkarththi15@gmail.com>
2023-11-13 12:14:39 +0530

    attendance Manager init changes

---
+++ src/test/java/com/attendance/Manager/ManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attendance.Manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
